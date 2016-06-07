bb4ef538205b9edd6ce984253f854b01d07adc7f
Christopher <cmullins@liveramp.com>
2016-06-06 22:59:19 -0700

    document puma config

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -4,7 +4,13 @@ require "#{root}/config/puma/environment"
 
 pidfile "#{root}/tmp/puma.pid"
 state_path "#{root}/tmp/puma.state"
-bind "unix://#{root}/tmp/puma.sock"
+
+# Comment this out and uncomment the following "bind" directive if you prefer
+# to listen on a unix socket
+bind "tcp://0.0.0.0:8000"
+
+# Uncomment this if you want to listen on a unix socket
+# bind "unix://#{root}/tmp/puma.sock"
 
 stdout_redirect "#{root}/log/puma.out", "#{root}/log/puma.err", true
 threads 4, 8
