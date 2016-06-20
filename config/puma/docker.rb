5a6b6ec97fc7c7bf42bb3a167ccb852cebc56fb1
Christopher <cmullins@liveramp.com>
2016-06-19 18:58:11 -0700

    use tcp socket

--- config/puma/docker.rb
+++ config/puma/docker.rb
@@ -2,7 +2,7 @@ root = File.expand_path(File.join(__FILE__, '../../..'))
 
 require "#{root}/config/puma/environment"
 
-bind "unix:///var/run/riddlegate/puma.sock"
+bind "tcp://0.0.0.0:8000"
 stdout_redirect "#{root}/log/puma.out", "#{root}/log/puma.err", true
 threads 4, 8
 
